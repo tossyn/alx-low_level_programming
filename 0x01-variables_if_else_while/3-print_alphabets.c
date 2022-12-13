6a031d6488dc851a1db7bedc1296316e809a7977
tossyn <tossyn02@yahoo.com>
2022-12-13 04:29:19 +0000

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: 0 on sucess
+*/
+
+int main(void)
+{
+	int s = 97;
+	int e = 122;
+
+	while (s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+
+	s = 65;
+	e = 90;
+
+	whiule (s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
