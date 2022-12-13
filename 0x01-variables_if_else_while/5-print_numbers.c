1eaa54b9f03ccefa1332439717d1b09b9a071d87
tossyn <tossyn02@yahoo.com>
2022-12-13 04:57:24 +0000

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Return: 0 on sucess
+*/
+
+int main(void)
+{
+	int s = 0;
+	int e = 9;
+
+	while (s <= e)
+	{
+		putchar(s + '0');
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
