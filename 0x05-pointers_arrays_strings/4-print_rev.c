5897e624235175bcb71b28e64143f3d9a98462ec
tossyn <tossyn02@yahoo.com>
2022-12-19 13:02:49 +0000

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse
+ * @s: string to print in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
