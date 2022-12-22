df09cb1bbbaf74191cb3c02c47f7ac145b46db9b
tossyn <tossyn02@yahoo.com>
2022-12-22 06:55:22 +0000

    update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: char first string
+ * @src: char second string
+ * Return: char concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This the the README file for '0x06-pointers_arrays_strings' project.
